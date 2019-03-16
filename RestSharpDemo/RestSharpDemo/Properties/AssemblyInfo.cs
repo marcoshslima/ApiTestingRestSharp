4207b3df4e38811feb4ff8d74b1d102f8c491c9c
marcoshslima <marcoshslima@gmail.com>
2019-03-16 19:02:37 -0300

    Primeiras Aulas com RestSharp

---
+++ RestSharpDemo/RestSharpDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RestSharpDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RestSharpDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("82919a2f-ea17-4687-88b2-0e62c3af9a54")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RestSharpDemo/RestSharpDemo/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using RestSharp;
+using RestSharp.Serialization.Json;
+
+namespace RestSharpDemo
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+
+            var client = new RestClient("http://localhost:3000/");
+            var request = new RestRequest("posts/{postid}",Method.GET);
+            request.AddUrlSegment("postid", 1);
+
+            //var content = client.Execute(request).Content;
+            //Dois jeitos para pegar o valor do Json
+
+            var response = client.Execute(request);
+
+            var deseriable = new JsonDeserializer();
+            //var output = deseriable.Deserialize<Dictionary<string, string>>(response);
+            // var result = output["author"];
+            //Assert.That(result, Is.EqualTo("typicode"), "Author is incorret");
+
+            JObject objs = JObject.Parse(response.Content);
+            Assert.That(objs["author"].ToString(), Is.EqualTo("typicode"), "Author is incorret");
+
+        }
+    }
+}
